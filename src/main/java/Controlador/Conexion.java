f7f089b023c998346344c7fa54dc16182420a019
AgustQZ <san.agustin20.aqz@gmail.com>
2021-10-14 04:30:29 -0500

    conexion a maria db

--- src/main/java/Controlador/Conexion.java
+++ src/main/java/Controlador/Conexion.java
@@ -6,15 +6,15 @@ import java.sql.DriverManager;
 import javax.swing.JOptionPane;
 
 public class Conexion {
-	private String url = "jdbc:mysql://localhost:3306/tiendagenerica";
-	private String user = "root";
-	private String pass = "SanagustinSQL.";
+	private String url = "jdbc:mariadb://prestamosvf.czo3ixoe3xoe.us-east-1.rds.amazonaws.com/Grupo44_Equipo_2";
+	private String user = "admin";
+	private String pass = "Ciclo3_admi123";
 	
 	Connection conexion= null;
 	
 	public Connection hacerConexion(){
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver");
 			conexion = DriverManager.getConnection(url, user, pass);		
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Error al realizar la conexion"+e);
